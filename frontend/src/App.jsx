b8f6240efe98b089fb3f038ec33e38bce91692cd
Cihan <ozcancihan714@gmail.com>
2026-09-11 13:49:57 +0300

    fix: add basename to Router for GitHub Pages subdirectory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ToastProvider>
         <Main token={token} user={user} login={login} logout={logout} />
       </ToastProvider>
